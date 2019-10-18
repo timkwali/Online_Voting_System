314695bcdfca0bd044dab6286df40e92e2c731b1
Timring <timringperez@gmail.com>
2019-10-18 16:40:00 +0100

    completed update candidate functonality

--- js/adminPage.js
+++ js/adminPage.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   const registerCandidate = () => {
     event.preventDefault();
     if($("#age").val() < 18) {
@@ -19,6 +20,7 @@ $(document).ready(function() {
         url:"http://localhost:3000/candidates",
         data: candidate,
         success: function() {
+          document.location.reload()
           alert("Successfully registered candidate");
         },
         error: function() {
@@ -41,7 +43,6 @@ $(document).ready(function() {
           $(".names").append(name.clone())
           $(".info").css("background-image", `url(images/inecLogo2.jpg)`)
           $(".info").css("background-size", `cover`)
-
         }) 
         showCandidateDetails(candidates);
       },
@@ -73,7 +74,7 @@ $(document).ready(function() {
             $("#updDel").text("")
             $("#updDel").append(updateBtn);
             $("#updDel").append(deleteBtn);
-            //show about candidate info
+            //show candidate info
             $("#bottom").css("border-top", "3px solid rgb(21, 214, 0)")
             let p = $("<p/>", {"class": "inf"});
             $("#bottom").text("")
@@ -94,18 +95,18 @@ $(document).ready(function() {
         })
       })
     })
-  }
 
+  }
 
   const deleteCandidate = (candidates, button) => {
     candidates.forEach(candidate => {
       if(button.innerHTML === `${candidate["firstName"]} ${candidate["lastName"]}`) {
-        if( confirm(`ARE YOU SURE YOU WANT TO DELETE ${candidate["firstName"]} ${candidate["lastName"]}?`) ) {
+        if( confirm(`Are you sure you want to delete ${candidate["firstName"]} ${candidate["lastName"]}?`) ) {
           $.ajax({
             type: "DELETE",
             url: `http://localhost:3000/candidates/${candidate["id"]}`,
             success: function(data) {
-              alert(`YOU DELETED ${candidate["firstName"]} ${candidate["lastName"]}!`)
+              alert(`You deleted ${candidate["firstName"]} ${candidate["lastName"]}!`)
               $("#candPic").css("background-image", "none");
               $("#partyPic").css("background-image", "none");
               $("#updDel").text("");
@@ -127,56 +128,159 @@ $(document).ready(function() {
 
   const updateCandidate = (candidates, button) => {
     candidates.forEach(candidate => {
-      
+      if(button.innerHTML === `${candidate["firstName"]} ${candidate["lastName"]}`) {
+        $("#candidates").hide();
+        $("#updCandidate").show().css("margin-top", "0%");
+        $("#updateFirstName").val(candidate["firstName"]);
+        $("#updateLastName").val(candidate["lastName"]);
+        $("updateAge").val( parseInt(candidate["age"]) );
+        $("updateParty").val(candidate["party"]);
+        $("updateGender").val(candidate["gender"]);
+        $("updateCandidateImage").text(candidate["candidateImage"]);
+        $("updatePartyImage").val(candidate["partyImage"]);
+      }
+    })
+    $("#updateCandidateButton").click(function() {
+      updateCandidateData(candidates, button);
     })
   }
 
-  //show/hide different windows
-  const showRegCand = () => {
-    $("#candidates").hide();
-    $("#deadline").hide();
-    $("#winner").hide();
-    $(".signIn").show();
-  }
-  const showCand = () => {
-    $(".signIn").hide();
-    $("#deadline").hide();
-    $("#winner").hide();
-    $("#candidates").show();
-  }
-  const showSetDead = () => {
-    $("#candidates").hide();
-    $(".signIn").hide();
-    $("#winner").hide();
-    $("#deadline").show();
-  }
-  const showViewWin = () => {
-    $("#candidates").hide();
-    $("#deadline").hide();
-    $(".signIn").hide();
-    $("#winner").show();
+  const updateCandidateData = (candidates, button) => {
+    event.preventDefault();
+    if($("#updateAge").val() < 18) {
+      alert("Candidate must be 18 years and above!")
+    } else {
+      let updateData = {
+        "firstName": $("#updateFirstName").val().toUpperCase(),
+        "lastName": $("#updateLastName").val().toUpperCase(),
+        "age": $("#updateAge").val(),
+        "party": $("#updateParty").val(),
+        "gender": $("#updateGender").val(),
+        "candidateImage": $("#updateCandidateImage").val(),
+        "partyImage": $("#updatePartyImage").val(),
+        "votes": 0
+      }
+      console.log(button)
+      candidates.forEach(candidate => {
+        if(button.innerHTML === `${candidate["firstName"]} ${candidate["lastName"]}`) {
+          $.ajax({
+            type: "PUT",
+            url:`http://localhost:3000/candidates/${candidate["id"]}`,
+            data: updateData,
+            success: function() {
+              alert(`Successfully updated candidate ${updateData["firstName"]} ${updateData["lastName"]}`)
+              document.location.reload();
+            },
+            error: function() {
+              alert("Something went wrong!")
+            }
+          })
+        }
+      })
+    }
   }
 
-  //handle events
-  $("#submitCandidate").click(function() {
-    registerCandidate();
-  });
-  $(".update").click(function() {
-    $("#candidates").hide();
-    $("#updateCandidate").show();
-  })
- 
-  //toggle sidebar buttons
-  $("#regCand").click(function() {
-    showRegCand();
-  })
-  $("#upCand").click(function() {
+  const resetformValues = () => {
+    $("#updateFirstName").val("");
+    $("#updateLastName").val("");
+    $("#updateAge").val("");
+    $("#updateParty").val("political party");
+    $("#updateGender").val("Gender");
+    $("#updateCandidateImage").val("");
+    $("#updatePartyImage").val("");
+  }
+
+  const resetCandidatesCSS = () => {
     $("#candPic").css("background-image", "none");
     $("#partyPic").css("background-image", "none");
     $("#updDel").text("");
     $("#bottom").text("").css("border-top", "none")
     $(".names").text("")
+  }
+
+  const checkElectionResult = () => {
+    event.preventDefault();
+    $.ajax({
+      type: "GET",
+      url: `http://localhost:3000/candidates`,
+      success: function(candidates) {
+        let mostVotes = 0;
+        let winner= [];
+        //get most number of received votes
+        candidates.forEach(candidate => {
+          if( parseInt(candidate["votes"]) > mostVotes) {
+            mostVotes = parseInt(candidate["votes"]);
+          }  
+        })
+        //get the candidate(s) with the most votes
+        candidates.forEach(candidate => {
+          if( parseInt(candidate["votes"]) === mostVotes) {
+            winner.push(`${candidate["firstName"]} ${candidate["lastName"]}`)
+          }
+        })
+        //display election results
+        let p = $("<p/>", {"class": "inf"});
+        candidates.forEach(candidate => {
+          $("#results").text("");
+          $("#results").append(p.text(`${candidate["firstName"]} ${candidate["lastName"]}: ${candidate["votes"]} VOTES`));
+        })
+
+
+        //declare winner(s) of the election
+        if(winner.length === 1) {
+          alert(`The winner of the election is ${winner[0]}`)
+        }
+      },
+      error: function() {
+        alert("Something went wrong")
+      }
+    })
+  }
+
+  //show/hide different windows
+  const showRegCand = () => {
+    $("#updCandidate").hide();
+    $("#candidates").hide();
+    $("#deadline").hide();
+    $("#winner").hide();
+    $("#regCandidate").show();
+  }
+  const showCand = () => {
+    $("#updCandidate").hide();
+    $("#regCandidate").hide();
+    $("#deadline").hide();
+    $("#winner").hide();
+    $("#candidates").show();
+  }
+  const showSetDead = () => {
+    $("#updCandidate").hide();
+    $("#candidates").hide();
+    $("#regCandidate").hide();
+    $("#winner").hide();
+    $("#deadline").show();
+  }
+  const showViewWin = () => {
+    $("#updCandidate").hide();
+    $("#candidates").hide();
+    $("#deadline").hide();
+    $("#regCandidate").hide();
+    $("#winner").show();
+  }
+
+
+  //handle events
+  $("#submitCandidate").click(function() {
+    registerCandidate();
+  });
+ 
+  //toggle sidebar buttons
+  $("#regCand").click(function() {
+    document.location.reload();
+  })
+  $("#upCand").click(function() {
+    resetCandidatesCSS();
     showCand();
+    resetformValues();
     viewCandidates();
   })
   $("#setDead").click(function() {
@@ -184,6 +288,7 @@ $(document).ready(function() {
   })
   $("#viewWin").click(function() {
     showViewWin();
+    checkElectionResult();
   })
   $("body").on("load", showRegCand());
 })
